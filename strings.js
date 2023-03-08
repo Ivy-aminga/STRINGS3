f4ffc843af8a195e248ce11ede9000fd3a7c3879
Ivy-aminga <amingaivy@gmail.com>
2023-03-08 12:50:19 +0300

    STRINGS3

--- strings.js
+++ strings.js
@@ -1,29 +1,27 @@
 // Extract the last four characters from the string below;
-//"extravaganza"
-
-let y = "extravaganza"
-console.log(y[8],y[9],y[10],y[11]);
-
+// "extravaganza"
+var b = "extravaganza"
+var lastfour = b.slice(-4);
+console.log(lastfour);
 
 // Insert the following string at the fourth index of the below variable:
 // "eat"
 // const food = "The quick fox jumped over the lazy dog"
 
-const a = "eat"
-const food = "The quick fox jumped ${a}the lazy dog"
-console.log(food);
+const food = "The quick fox jumped over the lazy dog";
+var hot = food.slice(0,4)+"eat"+ food.slice(4);
+console.log(hot);
 
 // Count how many times the following string appears in the string variable:
 // 1. "the"
 // 2."brown"
 // const story= "The quick brown fox jumps over the lazy dog"
 
-const story = "The quick brown fox jumps over the lazy dog "
-const quick = (story.match(/the/g)||[]).length;
-const fox =  (story.match(/brown/g)||[]).length;
-console.log(quick);
-console.log(fox);
+const story = "The quick brown fox jumps over the lazy dog";
+const count = (story.match(/the/g) || []).length;
+console.log(count);
 
+const count1 = (story.match(/brown/g) || []).length;
 
 // Using JavaScript, find the following words from the following strings:
 // 1. "are"
@@ -32,31 +30,30 @@ console.log(fox);
 // const string2 = "The child was sitting on the table before it fell"
 
 const string1 = "The pupils are reading in the library";
-const library = string1.search ("are");
-console.log(library);
+let find= string1.search("are");
+console.log(find);
 
-const string2 ="The child was sitting on the table before it fell"
-const child=string2.search("sitting");
-console.log(child);
-
+const string2 = "The child was sitting on the table before it fell";
+let look = string2.search("sitting");
+console.log(look);
 
 // Convert the following strings into the specified format:
 // 1. UpperCase: "wonderful"
 // 2. LowerCase: "amazing", "UndERneath"
 // 3. Title case "A wonderful world"
 
-const up = "wonderful";
-const upper = up.toUpperCase();
-console.log(upper);
+const word1 = "wonderful";
+let result = word1.toUpperCase("wonderful");
+console.log(result);
 
-const low = "amazing";
-const lower = low.toLowerCase();
-console.log(lower);
+const word2 = "amazing";
+const word3 = "UndERneath"
+let convert = word2.toLowerCase("amazing");
+let convert1 = word3.toLowerCase("UndERneath");
 
-const small = "BEneath";
-const smaller = small.toLowerCase();
-console.log(lower);
+console.log(convert);
+console.log(convert1);
 
-const title = "A wonderful world";
-const titlee = title.toLocaleUpperCase();
-console.log(titlee);
+let word4 = "A wonderful world";
+word4=word4.split(" ").map(([firstChar,...rest])=>firstChar.toUpperCase()+rest.join("").toLowerCase()).join(" ");
+console.log(word4);
